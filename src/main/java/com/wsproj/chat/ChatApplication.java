bd3cade1883938cce2e0573dc983903e3d4f906c
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2026-05-30 13:52:13 -0400

    add project dependencies

---
+++ src/main/java/com/wsproj/chat/ChatApplication.java
@@ -0,0 +1,15 @@
+package com.wsproj.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChatApplication.class, args);
+		System.out.println("Hiii :D");
+	}
+
+}
